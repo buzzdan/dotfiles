22428878d135b41bdbfb768d9116470dd8364d51
Dan Mordechay <danfromisrael@gmail.com>
2021-08-10 10:01:19 +0300

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo chown -R "$(whoami)" "$(brew --prefix)"/*
 brew doctor
 brew update
-brew cask install homebrew/cask-versions/adoptopenjdk8
+
+# install java
+curl -s "https://get.sdkman.io" | bash
+sdk i java 11.0.11.hs-adpt
+
+# terminal power tools
 brew install zsh
 brew install git git-extras lazygit tree htop zsh-syntax-highlighting clojure leiningen z shellcheck nvm tldr fortune cowsay lolcat asciiquarium
-brew cask install iterm2 visual-studio-code docker postman charles spectacle clipy
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-touch /etc/environment
+# install oh-my-zsh and make zsh the default terminal
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s "$(which zsh)"
+
+touch /etc/environment # inteliji tweek to load env vars --> not sure its working
+
+# add to .zshrc to load on every terminal load
 {
     echo 'plugins=(common-aliases extract z git git-extras mercurial docker lein yarn npm colored-man-pages aws battery golang osx brew brew-cask zsh-syntax-highlighting catimg chucknorris)'
     echo 'source /etc/environment'
@@ -23,5 +33,10 @@ touch /etc/environment
     echo 'COMPLETION_WAITING_DOTS="true"'
 } >> ~/.zshrc
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s "$(which zsh)"
+
+
+
+
+# UI power tools
+brew install --cask iterm2 visual-studio-code docker postman charles spectacle clipy
+
